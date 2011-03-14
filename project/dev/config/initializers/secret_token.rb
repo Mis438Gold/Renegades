d1062a80ca00696af1e62c23cd08a0b472f7a221
MIS438Gold <goldgroup@iastate.edu>
2011-03-14 14:10:14 -0700

    initial commit

---
+++ project/dev/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dev::Application.config.secret_token = 'a288888cff897a7bf040d1675f7c9c93ceb31eb8386834bb121736b607a4ac47ad461e39c55785e1af80f68a3e965c3a2d7b0140c40ea501f2f6094a94a39ca8'
